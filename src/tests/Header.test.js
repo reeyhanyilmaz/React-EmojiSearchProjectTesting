a723c9b9d7e1dbc3d70bfa7f438e45fed3c3f68d
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-04-12 17:30:03 +0300

    a few installing and add header.test

---
+++ src/tests/Header.test.js
@@ -0,0 +1,12 @@
+import { render} from "@testing-library/react";
+import React from 'react';
+import Header from "../Header";
+
+describe("Header Tests" , () => {
+
+    test("header" , () => {
+        render(<Header />);
+
+    })
+
+});
